23344e28cce5dd459d4570508b3f9b309a87ae33
Jeroen Bourgois <jeroen@jackjoe.be>
2022-02-24 21:06:26 +0100

    chore: tweaks

--- src/dwmstatus.c
+++ src/dwmstatus.c
@@ -24,19 +24,25 @@
 #include <fcntl.h>
 #include <X11/Xlib.h>
 
-#define MOUSE_FILE "/dev/input/event23"
+void set_status(Display *display, Window window, char *str);
+char* get_mem_usage();
+char* get_date_time();
+char* get_disk_usage(const char *path);
+void process_mouse(Display *display, XEvent xevent);
 
-void setStatus(Display *dpy, char *str);
-double getMemUsed();
-char* getDateTime();
-char * getDiskUsage(const char *path);
-void processMouse(int mfd, struct input_event ie, Display *dpy);
+char *key_name[] = {
+    "first",
+    "second (or middle)",
+    "third"
+};
 
 int 
 main(void) 
 {
   const int MSIZE = 1024;
-  Display *dpy;
+  Display *display;
+  XEvent xevent;
+  Window window;
   char *status;
   char *bg_color    = "#000000";
   // char *clr_green   = "#bbd5bd";
@@ -47,91 +53,103 @@ main(void)
   char *disk_home_free;
   char *disk_sys_free;
   char *datetime;
-  int mfd;
-  struct input_event ie;
-  double mem_used;
+  // int mfd;
+  // struct input_event ie;
+  char *mem_usage;
+  time_t previousTime = time(NULL);
+  time_t interval_status = 1;
+  time_t currentTime;
 
-  if (!(dpy = XOpenDisplay(NULL))) {
+  if (!(display = XOpenDisplay(NULL))) {
     fprintf(stderr, "Cannot open display.\n");
     return EXIT_FAILURE;
   }
 
-  if((mfd = open(MOUSE_FILE, O_RDONLY)) == -1) {
-    perror("Opening mouse device");
-    return EXIT_FAILURE;
-  }
+  // Setup X11 for mouse grabbing
+  window = DefaultRootWindow(display);
+  XAllowEvents(display, AsyncBoth, CurrentTime);
+
+  XGrabPointer(display, 
+      window,
+      1, 
+      PointerMotionMask | ButtonPressMask | ButtonReleaseMask , 
+      GrabModeAsync,
+      GrabModeAsync, 
+      None,
+      None,
+      CurrentTime);
 
   status = (char*) malloc(sizeof(char)*MSIZE);
   if(!status)
     return EXIT_FAILURE;
 
-  processMouse(mfd, ie, dpy);
-
   while(1)
   { 
-    mem_used = getMemUsed(); 
-    datetime = getDateTime();
-    disk_home_free = getDiskUsage("/home");
-    disk_sys_free = getDiskUsage("/"); 
+    // process_mouse(display, xevent);
+    if((time(&currentTime) - previousTime) >= interval_status)
+    {
+      mem_usage = get_mem_usage(); 
+      datetime = get_date_time();
+      disk_home_free = get_disk_usage("/home");
+      disk_sys_free = get_disk_usage("/"); 
 
-    int ret = snprintf(
-        status, 
-        MSIZE, 
-        "^b%s^^c%s^Mem %f |  %s  %s | ^c%s^%s", 
-        bg_color,
-        clr_yellow,
-        mem_used,
-        disk_sys_free,
-        disk_home_free,
-        clr_blue,
-        datetime
-        );
-    if(ret >= MSIZE)
-      fprintf(stderr, "error: buffer too small %d/%d\n", MSIZE, ret);
-    setStatus(dpy, status);
-    sleep(1);
+      int ret = snprintf(
+          status, 
+          MSIZE, 
+          "^b%s^^c%s^%s |  %s  %s | ^c%s^%s", 
+          bg_color,
+          clr_yellow,
+          mem_usage,
+          disk_sys_free,
+          disk_home_free,
+          clr_blue,
+          datetime
+          );
+      if(ret >= MSIZE)
+        fprintf(stderr, "error: buffer too small %d/%d\n", MSIZE, ret);
+      set_status(display, window, status);
+      previousTime += interval_status;
+    }
   }
+
+  return 0;
 }
 
 void 
-setStatus(Display *dpy, char *str) 
+set_status(Display *display, Window window, char *str) 
 {
-  XStoreName(dpy, DefaultRootWindow(dpy), str);
-  XSync(dpy, False);
+  XStoreName(display, window, str);
+  XSync(display, False);
 }
 
 // unsigned long
-double
-getMemUsed()
+char *
+get_mem_usage()
 {
+  char *buf;
   struct sysinfo si; 
   int error = sysinfo(&si);
+  buf = (char*) malloc(sizeof(char)*65);
   if(error == 0) {
-    /* printf("totalram: %lu\n", si.totalram);
-    printf("freeram: %lu\n", si.freeram);
-    printf("mem_unit: %u\n", si.mem_unit); */
-    si.mem_unit*=1024.0f;
-    si.mem_unit*=1024.0f;
     const unsigned int GB = 1024 * 1024 * 1024;
-    return (double)(si.totalram-si.freeram-(si.bufferram+si.sharedram))/GB; 
-    // return ((si.totalram-si.freeram)*si.mem_unit)/1024/1024;
+    const double used = (double)(si.totalram-si.freeram-(si.bufferram+si.sharedram))/GB; 
+    const char* color = "#ff00ff";
+    sprintf(buf, "^c%s^MEM %.1f GB", color, used);
   } else {
-    return -1;
+    buf = "";
   }
+
+  return buf;
 }
 
 char *
-getDateTime() 
+get_date_time() 
 {
   char *buf;
   time_t result;
   struct tm *resulttm;
   
-  if((buf = (char*) malloc(sizeof(char)*65)) == NULL)
-  {
-    fprintf(stderr, "Cannot allocate memory for buf.\n");
-    exit(1);
-  }
+  buf = (char*) malloc(sizeof(char)*65);
 
   result = time(NULL);
   resulttm = localtime(&result);
@@ -141,7 +159,7 @@ getDateTime()
       exit(1);
     }
   
-  if(!strftime(buf, sizeof(char)*65-1, "[%a %b %d] [%H:%M:%S]", resulttm))
+  if(!strftime(buf, sizeof(char)*65-1, "[%a %b %d (%H:%M:%S)]", resulttm))
     {
       fprintf(stderr, "strftime is 0.\n");
       exit(1);
@@ -151,7 +169,7 @@ getDateTime()
 }
 
 char *
-getDiskUsage(const char *path)
+get_disk_usage(const char *path)
 {
   char *buf;
 	struct statvfs fs;
@@ -172,47 +190,26 @@ getDiskUsage(const char *path)
   const double used = total - available;
   const double usedPct = (double)(used / total) * (double)100;
 
-  sprintf(buf, "%.0fG/%.0fG (%.0f%)", used, available, usedPct);
+  sprintf(buf, "%.0fGB / %.0fGB (%.0f%%)", used, available, usedPct);
 
 	return buf;
 }
 
+// process_mouse(int mfd, struct input_event ie, Display *display)
 void
-processMouse(int mfd, struct input_event ie, Display *dpy)
+process_mouse(Display *display, XEvent xevent)
 {
-  Window root, child;
-  int rootX, rootY, winX, winY;
-  unsigned int mask;
-  // read
-  while(read(mfd, &ie, sizeof(struct input_event))) {
-    if (ie.type == EV_SYN) {
-      if (ie.code == REL_X || ie.code == REL_Y) {
-        XQueryPointer(dpy, DefaultRootWindow(dpy), &root, &child, &rootX, &rootY, &winX, &winY, &mask); 
-      }
-    } else if (ie.type == EV_KEY) {
-      switch (ie.code) {
-        case BTN_LEFT:
-          // printf("Left mouse button ");
-          break;
-        case BTN_RIGHT:
-          // printf("Right mouse button ");
-          break;
-        default:
-          break;
-      }
-      /* if (ie.value == 0)  printf("released!!\n");
-         if (ie.value == 1)  printf("pressed!!\n"); */
-      if(ie.code == BTN_LEFT && ie.value == 1) {
-        // check coords
-        if(rootY >= 0 && rootY < 20 && rootX > 3350 && rootX < 3470) {
-          char *cmd = "kitty -e htop";    
-          FILE *fp;
-          if ((fp = popen(cmd, "r")) == NULL) {
-            printf("Error opening pipe!\n");
-          }
-        }
-      }
-      // printf("x %d\ty %d\n", rootX, rootY);
-    }
+  XNextEvent(display, &xevent);
+
+  switch (xevent.type) {
+    case MotionNotify:
+      printf("Mouse move      : [%d, %d]\n", xevent.xmotion.x_root, xevent.xmotion.y_root);
+      break;
+    case ButtonPress:
+      printf("Button pressed  : %s\n", key_name[xevent.xbutton.button - 1]);
+      break;
+    case ButtonRelease:
+      printf("Button released : %s\n", key_name[xevent.xbutton.button - 1]);
+      break;
   }
 }
